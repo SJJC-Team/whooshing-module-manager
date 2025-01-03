da32334d7f584d816cf9e136fe7b8d8d991739b4
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-01-03 18:29:23 +0000

    V0.2.3.1 修复初始化脚本未正确卸载服务的问题

--- env_init/init.sh
+++ env_init/init.sh
@@ -7,6 +7,11 @@ g='\033[32m'
 b='\033[34m'
 n='\033[0m'
 
+# 授权 src 文件夹中的所有 sh 文件
+echo -e "${b}正在授权 src 文件夹中的所有 sh 文件...${n}"
+find "$(dirname "$0")" -type f -name "*.sh" -exec chmod +x {} \;
+echo -e "${g}授权完成${n}"
+
 echo -e "${b}Creating /root/configs directory...${n}"
 sudo mkdir -p /root/configs
 
