a84cbdf8d892fab1cb8b4edee1cfec33c4cbc1e5
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-01-03 19:09:26 +0000

    V0.2.3.8 update init.sh

--- env_init/init.sh
+++ env_init/init.sh
@@ -12,29 +12,24 @@ echo -e "${b}正在授权 src 文件夹中的所有 sh 文件...${n}"
 find "$(dirname "$0")" -type f -name "*.sh" -exec chmod +x {} \;
 echo -e "${g}授权完成${n}"
 
-echo -e "${b}Creating /root/configs directory...${n}"
 sudo mkdir -p /root/configs
-
-echo -e "${b}Changing to /root/configs directory...${n}"
 cd /root/configs
 
-echo -e "${b}Cloning the repository...${n}"
+echo -e "${b}克隆仓库...${n}"
 git clone https://github.com/SJJC-Team/whooshing-module-manager.git
-
-echo -e "${b}Changing to whooshing-module-manager/env_init directory...${n}"
 cd whooshing-module-manager/env_init
 
-echo -e "${b}Setting execute permissions for init_env.sh and uninstall.sh...${n}"
+echo -e "${b}设置 init_env.sh 和 uninstall.sh 的可执行权限...${n}"
 chmod +x src/init_all.sh
 chmod +x src/uninstall.sh
 
-echo -e "${b}Running uninstall.sh...${n}"
+echo -e "${b}运行卸载程序...${n}"
 sudo src/uninstall.sh
 
-echo -e "${b}Running init_all.sh...${n}"
+echo -e "${b}运行初始化程序...${n}"
 sudo src/init_all.sh
 
-echo -e "${b}Cleaning up...${n}"
+echo -e "${b}清理...${n}"
 sudo rm -rf /root/configs/whooshing-module-manager
 
-echo -e "${g}Environment initialization complete!${n}"
+echo -e "${g}环境初始化完成.${n}"
