887c797e6bd39d1b8869b146f1f7bc5135e8f602
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-01-13 17:00:46 +0800

    V0.2.6.1 初始化 swift 项目

---
+++ wsm-ctl/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "wsm-ctl",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.5.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+            name: "wsm-ctl",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser")
+            ]
+        )
+    ]
+)
