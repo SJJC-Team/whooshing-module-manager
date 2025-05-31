b169adba5946f27a0453189d08f7acc5cdd77abe
ChenLin-Wang <wangchenlin2001@icloud.com>
2025-05-31 18:34:26 +0800

    V2.1.7.1

---
+++ wsm-ctl/Sources/Yamls/Yaml+delete.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+extension Yaml.MODULE {
+    func delete(env: Env, depends: Depends, filePath: String) {
+
+    }
+}
+
+extension Yaml.PGSQL {
+    func create(module: String, env: Env, depends: Depends, filePath: String) throws {
+
+    }
+}
