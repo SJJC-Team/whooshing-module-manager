ee7562c55733d23d00bd8c57fb542287f603e058
ChenLin Wang <wangchenlin2001@icloud.com>
2025-07-09 22:02:47 +0800

    V2.2.3.1 更新依赖

--- wsm-ctl/Package.swift
+++ wsm-ctl/Package.swift
@@ -19,9 +19,8 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.10.0"),
         .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.9.0"),
         .package(url: "https://github.com/SJJC-Team/cloudflare-dns.git", .upToNextMajor(from: "1.0.7")),
-        .package(url: "https://github.com/SJJC-Team/whooshing-fluent.git", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-basic.git", .upToNextMajor(from: "1.2.3")),
-        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-pgsql.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/SJJC-Team/whooshing-fluent.git", .upToNextMajor(from: "1.0.2")),
+        .package(url: "https://github.com/SJJC-Team/whooshing.toolbox-pgsql.git", .upToNextMajor(from: "1.0.5"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -36,9 +35,6 @@ let package = Package(
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "CloudflareDNS", package: "cloudflare-dns"),
                 .product(name: "PgSQL", package: "whooshing.toolbox-pgsql"),
-                .product(name: "Cryptos", package: "whooshing.toolbox-basic"),
-                .product(name: "DataConvertable", package: "whooshing.toolbox-basic"),
-                .product(name: "ErrorHandle", package: "whooshing.toolbox-basic"),
                 .product(name: "AsyncHTTPClient", package: "async-http-client"),
                 .product(name: "_NIOFileSystem", package: "swift-nio")
             ],
